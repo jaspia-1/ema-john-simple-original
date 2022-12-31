b616980a78195d1b27ca7ce452c4da6363b13ce6
AspiaNew <taspiak049@gmail.com>
2023-01-01 00:16:59 +0600

    Display local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import "./Cart.css"
+const Cart = (props) => {
+    const { cart } = props
+    // console.log(cart)
+    let total = 0
+    let shipping = 0
 
-const Cart = ({ cart }) => {
+    for (const product of cart) {
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    let tax = total * 0.1;
+    const grandtotal = total + shipping + parseFloat(tax)
     return (
-        <div>
-            <h2>this is shop container</h2>
+        <div className='cart'>
+            <h2>Order Summary</h2>
             <p>Selected cart:{cart.length}</p>
+            <p>Total Price:${total}</p>
+            <p>Total Shipping Charge:${shipping}</p>
+            <p>Tax:{tax.toFixed(2)}</p>
+            <h5>Grand Total:{grandtotal.toFixed(2)}</h5>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import "./Shop.css"
@@ -7,17 +8,41 @@ const Shop = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
     useEffect(() => {
+        console.log("product launch before fetch")
         fetch("products.json")
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+
+                setProducts(data)
+                console.log('products loaded')
+            })
     }, [])
+    useEffect(() => {
+        console.log('first localstorage package', products)
+        const storedCart = getStoredCart()
+        const savedCart = []
+        for (const id in storedCart) {
+            const addProduct = products.find(product => product.id == id)
+            // console.log(addProduct)
+            if (addProduct) {
+                const quantity = storedCart[id]
+                addProduct.quantity = quantity
+                console.log(addProduct)
+                savedCart.push(addProduct)
+            }
+        }
+        setCart(savedCart)
+        console.log("finish local storage")
+    }, [products])
+
     // const handleClick = (product) => {
     //     console.log(product)
     // }
     const handleClick = (product) => {
-        console.log(product)
+        // console.log(product)
         const newArray = [...cart, product]
         setCart(newArray)
+        addToDb(product.id)
     }
 
     return (
